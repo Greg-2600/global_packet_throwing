f026d00c342aaccae4ccd17b2a091d9a2d2a82ca
gregbakerstl <gregbakerstl@gmail.com>
2019-08-07 20:01:20 +0000

    some comments

--- global_ping.sh
+++ global_ping.sh
@@ -2,11 +2,20 @@
 # ping the world
 
 lookup_host() {
+# given a domain, try DNS resolution, if it is found then return the IP and country
 	# function to perform a DNS lookup and pretty paint
 	local file_name="tld.clean" # name of file
-	local domain=$1             # domain comes in as arg
-	local ip=$(host -4 $domain|grep -v mail|grep "has address"|awk {'print $4'}|grep "[0-9]")
 
+	local domain=$1             # domain comes in as arg
+
+	# parse the result of ipv4 lookup and extract the ip if present
+	local ip=$(host -4 $domain|
+		grep -v mail|
+		grep "has address"|
+		awk {'print $4'}|
+		grep "[0-9]")
+
+	# if we have an ip, and a country, then return it
 	if [ "$ip" ]; then
 		local tld=$(echo $domain|tr "." " "|awk {'print "."$2'})
 		local country_name=$(grep $tld ${file_name})
@@ -16,12 +25,15 @@ lookup_host() {
 
 
 ping_host() {
+# given a domain as an agument, send an icmp echo request packet
 	local domain=$1
-	ping -t1 -c1 $domain|grep "64 bytes"
+	# send a single packet with a short timeout
+	ping -t2 -c1 $domain|grep "64 bytes"
 }
 
 
 domain_permutation() {
+# iterate through all of the tlds and prepend"www, nic, and gov"
 	local file_name="tld.clean"
 	while read tld; do
 		echo $tld|tr -d "\."|awk {'print $1"."$1'}
@@ -33,13 +45,18 @@ domain_permutation() {
 
 
 main() {
+	# randomly sort the domains
 	domain_permutation|sort -R|
 	while read domain; do 
+		# try to resolve domain
 		lookup_host $domain|while read line; do
+			# check if an IP address is associated in the result
 			local ip=$(echo $line|awk {'print $1'})		
+			# check the latency of a ping
 			local latency=$(ping_host $ip|awk {'print $7'}|sed 's/time=//g')
+			# if the packet came back, then paint the working result
 			if [ "$latency" ]; then
-				echo "$latency ms to $line"
+				echo "$latency ms to $line"|grep -v "0.0.0.0"
 			fi
 		done
 	done
